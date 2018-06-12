d2f0fd994038cef4a3f9dd39c93e8e6f3d3c5fba
friselis <svetlana.nazarenko.18@gmail.com>
2018-06-12 16:49:31 +0100

    -added icon for sending message

--- app/src/main/java/com/example/android/hackaton2018/ChatActivity.java
+++ app/src/main/java/com/example/android/hackaton2018/ChatActivity.java
@@ -5,12 +5,19 @@ package com.example.android.hackaton2018;
  */
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,18 +25,26 @@ import com.nexmo.sdk.conversation.client.Conversation;
 import com.nexmo.sdk.conversation.client.ConversationClient;
 import com.nexmo.sdk.conversation.client.Event;
 import com.nexmo.sdk.conversation.client.Text;
+import com.nexmo.sdk.conversation.client.audio.AppRTCAudioManager;
+import com.nexmo.sdk.conversation.client.audio.AudioCallEventListener;
 import com.nexmo.sdk.conversation.client.event.EventType;
 import com.nexmo.sdk.conversation.client.event.NexmoAPIError;
 import com.nexmo.sdk.conversation.client.event.RequestHandler;
 import com.nexmo.sdk.conversation.client.event.ResultListener;
 import com.nexmo.sdk.conversation.core.SubscriptionList;
+import static android.Manifest.permission.RECORD_AUDIO;
+
 
 public class ChatActivity extends AppCompatActivity {
     private final String TAG = ChatActivity.this.getClass().getSimpleName();
+    private static final int PERMISSION_REQUEST_AUDIO = 0;
+    private boolean AUDIO_ENABLED = false;
 
-    private TextView chatTxt;
-    private EditText msgEditTxt;
-    private Button sendMsgBtn;
+    private EditText chatBox;
+    private ImageButton sendBtn;
+    private TextView typingNotificationTxt;
+    private RecyclerView recyclerView;
+    private ChatAdapter chatAdapter;
 
     private ConversationClient conversationClient;
     private Conversation conversation;
@@ -40,32 +55,52 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        chatTxt = (TextView) findViewById(R.id.chat_txt);
-        msgEditTxt = (EditText) findViewById(R.id.msg_edit_txt);
-        sendMsgBtn = (Button) findViewById(R.id.send_msg_btn);
+        ConversationClientApplication application = (ConversationClientApplication) getApplication();
+        conversationClient = application.getConversationClient();
 
         Intent intent = getIntent();
+        String conversationId = intent.getStringExtra("CONVERSATION-ID");
+        System.out.println("conversationId: " + conversationId);
+        conversation = conversationClient.getConversation(conversationId);
+
+        chatBox = (EditText) findViewById(R.id.chat_box);
+        sendBtn = (ImageButton) findViewById(R.id.send_btn);
+        typingNotificationTxt = (TextView) findViewById(R.id.typing_notification);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        chatAdapter = new ChatAdapter(conversation);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        recyclerView.setAdapter(chatAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
         final String username = intent.getStringExtra("USERNAME");
         System.out.println("username: " + username);
 
-        sendMsgBtn.setOnClickListener(new View.OnClickListener() {
+        sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendMessage(username);
             }
         });
 
-        ConversationClientApplication application = (ConversationClientApplication) getApplication();
-        conversationClient = application.getConversationClient();
-        String conversationId = intent.getStringExtra("CONVERSATION-ID");
-        System.out.println("conversationId: " + conversationId);
-        conversation = conversationClient.getConversation(conversationId);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        addListener();
+      //  addListener();
+        attachListeners();
+    }
+
+    private void attachListeners() {
+        conversation.messageEvent().add(new ResultListener<Event>() {
+            @Override
+            public void onSuccess(Event result) {
+                chatAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(chatAdapter.getItemCount());
+            }
+        }).addTo(subscriptions);
     }
 
     @Override
@@ -74,43 +109,144 @@ public class ChatActivity extends AppCompatActivity {
         subscriptions.unsubscribeAll();
     }
 
-    private void sendMessage(String username) {
-        conversation.sendText(username + ": " + msgEditTxt.getText().toString(),
-                new RequestHandler<Event>() {
-            @Override
-            public void onSuccess(Event event) {
-                if (event.getType().equals(EventType.TEXT)) {
-                    Log.d(TAG, "onSent: " + ((Text) event).getText());
-                }
-            }
+//    private void sendMessage(String username) {
+//        conversation.sendText(username + ": " + chatBox.getText().toString(),
+//                new RequestHandler<Event>() {
+//            @Override
+//            public void onSuccess(Event event) {
+//                if (event.getType().equals(EventType.TEXT)) {
+//                    Log.d(TAG, "onSent: " + ((Text) event).getText());
+//                }
+//            }
+//
+//            @Override
+//            public void onError(NexmoAPIError apiError) {
+//                logAndShow("Error sending message: " + apiError.getMessage());
+//            }
+//        });
+//    }
 
+    private void sendMessage(String username) {
+        conversation.sendText(username + chatBox.getText().toString(), new RequestHandler<Event>() {
             @Override
             public void onError(NexmoAPIError apiError) {
                 logAndShow("Error sending message: " + apiError.getMessage());
             }
+
+            @Override
+            public void onSuccess(Event result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        chatBox.setText(null);
+                    }
+                });
+            }
         });
     }
 
-    private void addListener() {
-        conversation.messageEvent().add(new ResultListener<Event>() {
-            @Override
-            public void onSuccess(Event message) {
-                showMessage(message);
-            }
-        }).addTo(subscriptions);
-    }
 
-    private void showMessage(final Event message) {
-        if (message.getType().equals(EventType.TEXT)) {
-            Text text = (Text) message;
-            msgEditTxt.setText(null);
-            final String prevText = chatTxt.getText().toString();
-            chatTxt.setText(prevText + "\n" + text.getText());
-        }
-    }
+//    private void addListener() {
+//        conversation.messageEvent().add(new ResultListener<Event>() {
+//            @Override
+//            public void onSuccess(Event message) {
+//                showMessage(message);
+//            }
+//        }).addTo(subscriptions);
+//    }
+
+//    private void showMessage(final Event message) {
+//        if (message.getType().equals(EventType.TEXT)) {
+//            Text text = (Text) message;
+//            msgEditTxt.setText(null);
+//            final String prevText = chatTxt.getText().toString();
+//            chatTxt.setText(prevText + "\n" + text.getText());
+//        }
+//    }
 
     private void logAndShow(final String message) {
         Log.d(TAG, message);
         Toast.makeText(ChatActivity.this, message, Toast.LENGTH_SHORT).show();
     }
+
+    private void requestAudio() {
+        if (ContextCompat.checkSelfPermission(ChatActivity.this, RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            //TODO: implement
+            toggleAudio();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, RECORD_AUDIO)) {
+                logAndShow("Need permissions granted for Audio to work");
+            } else {
+                ActivityCompat.requestPermissions(ChatActivity.this, new String[]{RECORD_AUDIO}, PERMISSION_REQUEST_AUDIO);
+            }
+        }
+    }
+
+    private void toggleAudio() {
+        if(AUDIO_ENABLED) {
+            conversation.audio().disable(new RequestHandler<Void>() {
+                @Override
+                public void onError(NexmoAPIError apiError) {
+                    logAndShow(apiError.getMessage());
+                }
+
+                @Override
+                public void onSuccess(Void result) {
+                    AUDIO_ENABLED = false;
+                    logAndShow("Audio is disabled");
+                }
+            });
+        } else {
+            conversation.audio().enable(new AudioCallEventListener() {
+                @Override
+                public void onRinging() {
+                    logAndShow("Ringing");
+                }
+
+                @Override
+                public void onCallConnected() {
+                    logAndShow("Connected");
+                    AUDIO_ENABLED = true;
+                }
+
+                @Override
+                public void onCallEnded() {
+                    logAndShow("Call Ended");
+                    AUDIO_ENABLED = false;
+                }
+
+                @Override
+                public void onGeneralCallError(NexmoAPIError apiError) {
+                    logAndShow(apiError.getMessage());
+                    AUDIO_ENABLED = false;
+                }
+
+                @Override
+                public void onAudioRouteChange(AppRTCAudioManager.AudioDevice device) {
+                    logAndShow("Audio Route changed");
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_AUDIO: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO: implement
+                    toggleAudio();
+                    break;
+                } else {
+                    logAndShow("Enable audio permissions to continue");
+                    break;
+                }
+            }
+            default: {
+                logAndShow("Issue with onRequestPermissionsResult");
+                break;
+            }
+        }
+    }
+
 }
